137a620f32a20b798680a0b347ed145ac54fcf70
ptzafos <panagiotis.tzafos@gmail.com>
2018-07-29 16:54:39 +0200

    Version 1.0 of Decision Support

--- main.go
+++ main.go
@@ -4,9 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 )
 
-// balanceEur float32
+var balance float64
 
 type pandoraRequest struct {
 	Key   string
@@ -15,11 +16,28 @@ type pandoraRequest struct {
 	Rsi   float64
 }
 
-func decisionMaking() {}
+func decisionMaking(request pandoraRequest) {
+	macd := request.Macd
+	rsi := request.Rsi
+	close := request.Close
+	key := request.Key
+	if rsi <= 30.0 && macd < 0.0 {
+		buy(key, close)
+	}
+	if rsi >= 70.0 && macd > 0.0 {
+		sell(key, close)
+	}
+}
 
-func buy() {}
+func buy(key string, price float64) {
+	balance -= price
+	fmt.Println("Buy order:", key, "@", price, "- Current balance:", balance)
+}
 
-func sell() {}
+func sell(key string, price float64) {
+	balance += price
+	fmt.Println("Sell order:", key, "@", price, "- Current balance:", balance)
+}
 
 func postHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
@@ -29,8 +47,7 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "Error parsing request body", http.StatusBadRequest)
 			panic(parseErr)
 		}
-		msg, _ := json.Marshal(data)
-		fmt.Printf(string(msg))
+		decisionMaking(data)
 		fmt.Fprint(w, "Pandora Analytics Server: Request Received!")
 	} else {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
@@ -39,7 +56,11 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/pandora-analytics", postHandler)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
-	}
+	interrupt := make(chan os.Signal, 1)
+	go func() {
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			panic(err)
+		}
+	}()
+	<-interrupt
 }
